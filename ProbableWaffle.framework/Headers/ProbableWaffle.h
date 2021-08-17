2699be2da0e1c9bdd1dca5ef90b4a3178b634236
Nishan <nishan@ekoapp.com>
2021-08-17 11:24:30 +0700

    Add framework

---
+++ ProbableWaffle.framework/Headers/ProbableWaffle.h
@@ -0,0 +1,18 @@
+//
+//  ProbableWaffle.h
+//  ProbableWaffle
+//
+//  Created by Nishan Niraula on 8/17/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ProbableWaffle.
+FOUNDATION_EXPORT double ProbableWaffleVersionNumber;
+
+//! Project version string for ProbableWaffle.
+FOUNDATION_EXPORT const unsigned char ProbableWaffleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProbableWaffle/PublicHeader.h>
+
+
